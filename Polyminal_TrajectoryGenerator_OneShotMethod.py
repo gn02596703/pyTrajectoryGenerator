98b5b10b21352bc75d08e8be38362fb74d423b8a
YU <tjes50220@gmail.com>
2022-07-02 12:14:02 +0900

    Re-name spline parameter in code to match name in paper.

--- Polyminal_TrajectoryGenerator_OneShotMethod.py
+++ Polyminal_TrajectoryGenerator_OneShotMethod.py
@@ -1,5 +1,5 @@
 """
-The file can generate a smooth trajectory (a list of waypoint) according to 
+The algorithm generate a smooth trajectory (a list of waypoint) according to 
 given initial state and goal state.
 
 The algorithm is mainly based on following publications
@@ -7,11 +7,13 @@ The algorithm is mainly based on following publications
 Nagy and Kelly, 2001 
 "Adaptive Model Predictive Motion Planning for Navigation in Complex Environments",
 Thomas M. Howard, 2009, (chapter 4)
+"Parallel Algorithms for Real-time Motion Planning",
+Matthew McNaughton, 2011
 "Motion Planning for Self-Driving-Cars", Coursera 4 of Self-Driving-Car Specialization
-on Coursera
+in Coursera
 
-The implementation also references from Matthew O'Kelly's C++ implementation.
-You can find the source code in Autoware Repository.
+
+The implementation also references the idea from Matthew O'Kelly's C++ implementation in Autoware Repository.
 https://gitlab.com/autowarefoundation/autoware.ai/core_planning/-/blob/master/lattice_planner/lib/libtraj_gen.cpp
 """
 
@@ -39,14 +41,14 @@ class TrajectoryGenerator():
         self.pertub = 0.0001
 
         # path sampling resolution
-        self.sample_resolution = 1 # m
+        self.sample_resolution = 0.2 # m
     
     def _initialize_spline(self, initial_state, final_state):
         """
         The function initializes spline paramters 
 
         :param: final_state: goal of the path [x, y, theta, kappa]
-        :return: a, b, c, s: parameter of the spline
+        :return: p0, p1, p2, p3, s: parameter of the spline
         """
         x_f = final_state[0]
         y_f = final_state[1]
@@ -54,22 +56,16 @@ class TrajectoryGenerator():
         kappa_f = final_state[3] # rad
         kappa_0 = initial_state[3] # rad
 
+        # Initilalization method from 
+        # Parallel Algorithms for Real-time Motion Planning , Matthew McNaughton, 2011
         
-        d = np.sqrt(x_f**2 + y_f**2)
-        theta_delta = np.abs(theta_f)
-        s = d*(( (theta_delta**2) / 5) +1) + 2*theta_delta/5
+        p0 = kappa_0  # constrainted by initiali state
+        p1 = 0.0      # parameter to be optimized
+        p2 = 0.0      # parameter to be optimized
+        p3 = kappa_f  # constrainted by final state
+        s = x_f       # parameter to be optimized
         
-        # Initialization method from Nagy and Kelly, 2001 
-        # a = 6*theta_f/(s**2) - 2*kappa_0/s + 4*kappa_f/s
-        # c = 0
-        # b = 3*(kappa_0 + kappa_f)/(s**2) + 6*theta_f/(s**3)
-
-        # Initilalization method from Thomas M. Howard, 2009
-        a = 0.0
-        b = 0.0
-        c = kappa_f
-        
-        return a, b, c, s
+        return p0, p1, p2, p3, s
 
     def _compute_theta(self, theta, a_p, b_p, c_p, d_p, s):
         theta_final = theta + d_p*(s**4)/4 + \
@@ -110,32 +106,30 @@ class TrajectoryGenerator():
                                         4*np.sin(theta_7s_8) + np.sin(theta_s))* s/24.0
         return y_final
 
-    def _motion_update_one_shot(self, initial_state, a, b, c, s):
+    def _motion_update_one_shot(self, initial_state, p0, p1, p2, p3, s):
         """
         predict the final state according to initial state
         and spline parameter using one shot method
 
         :param initial_state: initial state of the vehcle [x, y, theta, kappa]
-        :param a: spline parameter
-        :param b: spline parameter
-        :param c: spline parameter
+        :param p0: spline parameter
+        :param p1: spline parameter
+        :param p2: spline parameter
+        :param p3: spline parameter
         :param s: spline parameter
         :return final_state_pred: predicted final state 
         """
         x_0 = initial_state[0]
         y_0 = initial_state[1]
-        theta = initial_state[2] # rad
-        kappa = initial_state[3] # rad
-        theta_0 = np.copy(theta)
-        kappa_0 = np.copy(kappa)
+        theta_0 = initial_state[2] # rad
+        kappa_0 = initial_state[3] # rad
 
-        a_p = kappa_0
-        b_p = -(11*kappa_0 - 18*a + 9*b - 2*c)/(2* s)
-        c_p = 9*(2*kappa_0 - 5*a + 4*b - c)/(2* s**2)
-        d_p = -9*(kappa_0 - 3*a + 3*b -c)/(2* s**3)
+        a_p = p0
+        b_p = -(11*p0 - 18*p1 + 9*p2 - 2*p3)/(2* s)
+        c_p = 9*(2*p0 - 5*p1 + 4*p2 - p3)/(2* s**2)
+        d_p = -9*(p0 - 3*p1 + 3*p2 -p3)/(2* s**3)
+
         kappa_final = a_p + b_p*s + c_p*(s**2) + d_p*(s**3)
-
-        
         theta_final = self._compute_theta(theta_0, a_p, b_p, c_p, d_p, s)
         x_final = self._compute_x(x_0, theta_0, a_p, b_p, c_p, d_p, s)
         y_final = self._compute_y(y_0, theta_0, a_p, b_p, c_p, d_p, s)
@@ -160,37 +154,32 @@ class TrajectoryGenerator():
 
         return converge
 
-    def _compute_correction(self, initial_state, final_state, a, b, c, s):
+    def _compute_correction(self, initial_state, final_state, p0, p1, p2, p3, s):
         """
         compute the correction of spline parameter
         """
         pertub = self.pertub
         pertub_s = pertub *10
     
-        pred_no_pertub = self._motion_update_one_shot(initial_state, a, b, c, s)
-        pred_pertub_a = self._motion_update_one_shot(initial_state, a +pertub, b, c, s)
-        pred_pertub_b = self._motion_update_one_shot(initial_state, a, b +pertub, c, s)
-        # no need to correct C, C is constrained by kappa_final
-        # # pred_pertub_c = self._motion_update_one_shot(initial_state, a, b, c +pertub, s)
-        pred_pertub_s = self._motion_update_one_shot(initial_state, a, b, c, s +pertub_s)
+        pred_no_pertub = self._motion_update_one_shot(initial_state, p0, p1, p2, p3, s)
+        pred_pertub_p1 = self._motion_update_one_shot(initial_state, p0, p1 +pertub, p2, p3, s)
+        pred_pertub_p2 = self._motion_update_one_shot(initial_state, p0, p1, p2 +pertub, p3, s)
+        pred_pertub_s = self._motion_update_one_shot(initial_state, p0, p1, p2, p3, s +pertub_s)
 
         d_state = np.zeros((3,1))
         d_pertub_state = np.zeros((3,3))
         Jacobian = np.zeros((3,3))
         for i in range(0, 3):
-            d_pertub_state[i][0] = (final_state[i] - pred_pertub_a[i]) # a
-            d_pertub_state[i][1] = (final_state[i] - pred_pertub_b[i]) # b
-            # d_pertub_state[i][2] = (final_state[i] - pred_pertub_c[i]) # c (no update)
+            d_pertub_state[i][0] = (final_state[i] - pred_pertub_p1[i]) # a
+            d_pertub_state[i][1] = (final_state[i] - pred_pertub_p2[i]) # b
             d_pertub_state[i][2] = (final_state[i] - pred_pertub_s[i]) # s
             
             d_state[i] = final_state[i] - pred_no_pertub[i]
-            
-            Jacobian[i][0] = (d_pertub_state[i][0] -  d_state[i])/pertub # a
-            Jacobian[i][1] = (d_pertub_state[i][1] -  d_state[i])/pertub # b
-            # Jacobian[i][2] = (d_pertub_state[i][2] -  d_state[i])/pertub # c (no update)
-            Jacobian[i][2] = (d_pertub_state[i][2] -  d_state[i])/pertub_s # s
 
-        # inv_Jacobian = np.linalg.inv(Jacobian)
+            Jacobian[i][0] = (pred_pertub_p1[i] -  pred_no_pertub[i])/pertub # p1
+            Jacobian[i][1] = (pred_pertub_p2[i] -  pred_no_pertub[i])/pertub # p2
+            Jacobian[i][2] = (pred_pertub_s[i] -  pred_no_pertub[i])/pertub_s # s
+
         inv_Jacobian = np.linalg.pinv(Jacobian)
         correction = np.dot(inv_Jacobian, d_state)
         # pdb.set_trace()
@@ -201,54 +190,47 @@ class TrajectoryGenerator():
         main function to compute the trajectory by optimizing an spline using
         initial and final state
         
-        Total parameter for a spline is
-        (kappa_0, a, b, c, s) --> equals (p0, p1, p2, p3, s) in papaer
+        Total parameter for a spline is (p0, p1, p2, p3, s)
         
-        Consider the constraints by start and goal point kappa, the parameter will be
-        (kappa_0, a, b, kappa_f, s) 
-        so, only need to estimate 3 parameters (a, b, s) 
+        Consider the constraints by start and goal point, the parameter will be
+        (kappa_0, p1, p2, kappa_f, s), we only need to optimize 3 parameters (p1, p2, s) 
         """
-        a, b, c, s = self._initialize_spline(initial_state, final_state)
-        final_state_pred = self._motion_update_one_shot(initial_state, a, b, c, s)
+        p0, p1, p2, p3, s = self._initialize_spline(initial_state, final_state)
+        final_state_pred = self._motion_update_one_shot(initial_state, p0, p1, p2, p3, s)
 
         converge = self._check_converge(final_state, final_state_pred)
         total_iter = 0
-        # pdb.set_trace()
-        while (total_iter < self.max_iter) & (converge is not True): # (total_iter < self.max_iter) 
+
+        while (total_iter < self.max_iter) & (converge is not True): 
             
+            correction = self._compute_correction(initial_state, final_state, p0, p1, p2, p3, s)
+            p1 = p1 + correction[0]
+            p2 = p2 + correction[1]
+            s = s + correction[2]
             
-            correction = self._compute_correction(initial_state, final_state, a, b, c, s)
-            a = a - correction[0]
-            b = b - correction[1]
-            # c = c - correction[2]
-            s = s - correction[2]
-            
-            final_state_pred = self._motion_update_one_shot(initial_state, a, b, c, s)
+            final_state_pred = self._motion_update_one_shot(initial_state, p0, p1, p2, p3, s)
 
             converge = self._check_converge(final_state, final_state_pred)
             total_iter = total_iter +1
 
-            # print(total_iter)
-            # print(final_state_pred)
-            # print(s)
-
         # sometimes it converge to negative s (travel distance) which 
         # is invalid..., need to figure it out...
         if (converge == True) & (s > 0):
-            final_state_pred, point_list = self._path_sampling_one_shot(initial_state, a, b, c, s)
+            final_state_pred, point_list = self._path_sampling_one_shot(initial_state, p0, p1, p2, p3, s)
         else:
             point_list = [[-1,-1]]
 
         return point_list
 
-    def _path_sampling_one_shot(self, initial_state, a, b, c, s):
+    def _path_sampling_one_shot(self, initial_state, p0, p1, p2, p3, s):
         """
         run through the spline and sample waypoint in a fix interval
 
         :param: initial_state: initial state of the vehcle [x, y, theta, kappa]
-        :param a: spline parameter
-        :param b: spline parameter
-        :param c: spline parameter
+        :param p0: spline parameter
+        :param p1: spline parameter
+        :param p2: spline parameter
+        :param p3: spline parameter
         :param s: spline parameter
         :return: final_state_pred: predicted final state 
         """
@@ -257,25 +239,21 @@ class TrajectoryGenerator():
         theta_0 = initial_state[2] # rad
         kappa_0 = initial_state[3] # rad
 
-        x = np.copy(x_0)
-        y = np.copy(y_0)
-        theta = np.copy(theta_0)
-        kappa = np.copy(kappa_0)
-
-        a_p = kappa_0
-        b_p = -(11*kappa_0 - 18*a + 9*b - 2*c)/(2* s)
-        c_p = 9*(2*kappa_0 - 5*a + 4*b - c)/(2* s**2)
-        d_p = -9*(kappa_0 - 3*a + 3*b -c)/(2* s**3)
+        a_p = p0
+        b_p = -(11*p0 - 18*p1 + 9*p2 - 2*p3)/(2* s)
+        c_p = 9*(2*p0 - 5*p1 + 4*p2 - p3)/(2* s**2)
+        d_p = -9*(p0 - 3*p1 + 3*p2 -p3)/(2* s**3)
         
-
         resolution = self.sample_resolution
 
         total_sample = int(s / resolution)
 
         point_list = []
         point_list.append([x_0, y_0])
-        for i in range(1, total_sample +1):
+        for i in range(1, total_sample +2):
             current_s = float(i) * resolution
+            if(current_s > s):
+                current_s = s
 
             kappa = a_p + b_p*current_s + c_p*(current_s**2) + d_p*(current_s**3)
             theta = self._compute_theta(theta_0, a_p, b_p, c_p, d_p, current_s)
@@ -306,9 +284,9 @@ def main():
     kappa_0 = 0.0 *np.pi/180  # initial steering angle  
     initial_state = [x_0, y_0, theta_0, kappa_0] 
     
-    x_f = 13.0 # final x position
-    y_f = 8.0 # final y position
-    theta_f = 0.0 *np.pi/180  # final heading angle of the vehicle 
+    x_f = 5.0 # final x position
+    y_f = 1.0 # final y position
+    theta_f = 20.0 *np.pi/180  # final heading angle of the vehicle 
     kappa_f = 0.0 *np.pi/180  # final steering angle  
     final_state = [x_f, y_f, theta_f, kappa_f] 
 
@@ -316,24 +294,22 @@ def main():
     point_array = np.asarray(traject)
     plt.plot(point_array[:,0], point_array[:,1],'o')
     
-    sample_resolution = 0.5
+    vertical_sample_resolution = 0.5
     temp_goal_list = []
-    for i in range(-2, 3):
+    for i in range(-5, 5):
         temp_final_state = np.copy(final_state)
-        temp_final_state[1] = temp_final_state[1] + float(i)*sample_resolution
+        temp_final_state[1] = temp_final_state[1] + float(i)*vertical_sample_resolution
         temp_goal_list.append(temp_final_state)
-        
     start = time.time()
     point_list = []
-    for i in range(0, 5):
+    for i in range(0, len(temp_goal_list)):
         temp_goal = temp_goal_list[i]
         traject = PathGenerator.compute_spline(initial_state, temp_goal)
         point_list.append(traject)
     end = time.time()
     print('Executed time is %f'%(end - start))
     
-    # pdb.set_trace()
-    for i in range(0,5):
+    for i in range(0, len(temp_goal_list)):
         point_array = np.asarray(point_list[i])
         plt.plot(point_array[:,0], point_array[:,1],'o')
     
